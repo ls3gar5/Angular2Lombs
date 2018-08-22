a43e2081c913a3492b054c1ff2767ba92e0df659
Leo <lsegars@gmail.com>
2018-08-22 07:32:12 -0300

    Fin sección 4

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { PortafolioComponent } from './Pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 //App Routing
 import { AppRoutingModulo } from './app-routing-module';
 
@@ -8,9 +8,10 @@ import { AppRoutingModulo } from './app-routing-module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { PortafolioComponent } from './Pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
+
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { ItemComponent } from './pages/item/item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModulo
+    AppRoutingModulo,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
