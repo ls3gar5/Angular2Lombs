4f11e4abbdef0e7c63c04ab73f52d9c326d4b839
Leo <lsegars@gmail.com>
2018-08-24 07:33:56 -0300

    Fin sección 5

--- src/app/interfaces/info-pagina-interface.ts
+++ src/app/interfaces/info-pagina-interface.ts
@@ -9,3 +9,15 @@ export interface InfoPagina {
   instagram: string;
   equipo_trabajo: any[];
 }
+
+export interface InfoEquipo {
+
+    frase: string;
+    nombre: string;
+    subtitulo: string;
+    twitter: string;
+    url: string;
+  }
+
+
+
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { InfoPagina } from '../interfaces/info-pagina-interface';
+import { InfoPagina, InfoEquipo } from '../interfaces/info-pagina-interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,22 +9,33 @@ import { InfoPagina } from '../interfaces/info-pagina-interface';
 export class InfoPaginaService {
 
   info: InfoPagina;
-  cargada: boolean =false;
+  cargada = false;
+  infoEquipo: InfoEquipo;
 
-  constructor(private http:HttpClient) { 
+  constructor(private http: HttpClient) {
+    // console.log('Servicio InfoPagina')
 
-    //console.log('Servicio InfoPagina')
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-    //Leer JSON
+  private cargarInfo() {
+    // Leer JSON
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
 
       this.cargada = true;
-      this.info= resp;
-      
-      console.log(resp); //resp es sin tipar por lo que no sabe q es.
+      this.info = resp;
+      console.log(resp); // resp es sin tipar por lo que no sabe q es.
     });
+  }
 
-
+  private cargarEquipo() {
+       // Leer JSON
+       this.http.get('https://lomb-bags.firebaseio.com/equipo.json')
+       .subscribe( (resp: InfoEquipo) => {
+         this.infoEquipo = resp;
+         console.log(resp); // resp es sin tipar por lo que no sabe q es.
+       });
   }
 }
